52290e57c29eea9210b3e7c8cfa90b023ad1a789
Rustams <saidrustam@mail.ru>
2021-01-25 20:36:20 +0500

    25-01-2021

--- src/js/script.js
+++ src/js/script.js
@@ -18,15 +18,17 @@ $(document).ready(function(){
             {
               breakpoint: 600,
               settings: {
-                slidesToShow: 2,
-                slidesToScroll: 2
+                slidesToShow: 1,
+                slidesToScroll: 1,
+                arrows: false
               }
             },
             {
               breakpoint: 480,
               settings: {
                 slidesToShow: 1,
-                slidesToScroll: 1
+                slidesToScroll: 1,
+                arrows: false
               }
             }
             // You can unslick at a given breakpoint now by adding:
@@ -34,5 +36,27 @@ $(document).ready(function(){
             // instead of a settings object
           ]
       });
+
+      $('ul.catalog__tabs').on('click', 'li:not(.catalog__tab_active)', function() {
+        $(this)
+          .addClass('catalog__tab_active').siblings().removeClass('catalog__tab_active')
+          .closest('div.container').find('div.catalog__content').removeClass('catalog__content_active').eq($(this).index()).addClass('catalog__content_active');
+      });
+
+      function toggleSlide(item) {
+        $(item).each(function(i) {
+          $(this).on('click', function(e) {
+            e.preventDefault();
+            $('.catalog-item__content').eq(i).toggleClass('catalog-item__content_active');
+            $('.catalog-item__list').eq(i).toggleClass('catalog-item__list_active');
+          })
+        });
+
+      };
+
+      toggleSlide('.catalog-item__link');
+      toggleSlide('.catalog-item__back');
+
+
   });
           
