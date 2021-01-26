cb53e6607d5e3bfbbd9b2a0d22eb3bd972da8a60
Rustams <saidrustam@mail.ru>
2021-01-26 16:01:43 +0500

    26_01_2021

--- src/js/script.js
+++ src/js/script.js
@@ -57,6 +57,53 @@ $(document).ready(function(){
       toggleSlide('.catalog-item__link');
       toggleSlide('.catalog-item__back');
 
+      //Modal windows
+
+      $('[data-modal=consultation]').on('click', function() {
+        $('.overlay, #consultation').fadeIn('slow');
+      });
+
+      $('.modal__close').on('click', function() {
+        $('.overlay, #consultation, #order, #thanks').fadeOut('slow');
+      });
+
+      $('.button_mini').each(function(i) {
+          $(this).on('click', function () {
+            $('#order .modal__descr').text($('.catalog-item__subtitle').eq(i).text());
+            $('.overlay, #order').fadeIn('slow');
+          })
+     
+      });
+
+      $('.consultation form').validate();
+      $('#consultation form').validate({
+        rules: {
+          name: {
+            required: true,
+            minlength: 2
+          },
+          phone: "required",
+          email: {
+            required: true,
+            email: true
+          }
+
+        },
+        messages: {
+          name: {
+            required: "Пожалуйста, введите свое имя",
+            minlength: jQuery.validator.format("Введите {0} символа!")
+          },
+          phone: "Пожалуйста, введите свой номер телефон",
+          email: { 
+            required: "Пожалуйста, введите свою почту",
+            email: "Неправильно введен адрес почты"
+        }        
+      }
+      });
+      $('#order form').validate();
+      
+
 
   });
           
